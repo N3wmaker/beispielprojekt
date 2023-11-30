d5a801aa640e81c51848f0338923791ab06b67a9
Newmaker <newmaker@live.de>
2023-11-30 09:12:29 +0000

    first commit

---
+++ tsdev/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+class Main {
+    static sayHello(n) {
+        for (let i = 0; i < n; i++)
+            console.log("Hello World");
+    }
+}
+Main.sayHello(3);
+//# sourceMappingURL=index.js.map
